b4f13cec1467cc7378a8bc8ce4ae8c6a289ce03f
KristiaKnt <cristian.rojascapera5@gmail.com>
2023-08-22 23:43:58 -0500

    video numero 15 y 16

--- js/script.js
+++ js/script.js
@@ -1,36 +1,56 @@
-class ListaTareas{
-    constructor(){
-        this.tareas=JSON.parse(localStorage.getItem('tareas'));
+class ListaTareas {
+    constructor() {
+        this.tareas = JSON.parse(localStorage.getItem('tareas'));
 
-        if(!this.tareas){
-            this.tareas=[
-                {tarea: 'Aprender JavaScript',
-                completado: false},
-                {tarea: 'Aprender python',
-                completado: false},
-                {tarea: 'Aprender c++',
-                completado: true},
-                
+        if (!this.tareas) {
+            this.tareas = [
+                {
+                    tarea: 'Aprender JavaScript',
+                    completado: false
+                },
+                {
+                    tarea: 'Aprender python',
+                    completado: false
+                },
+                {
+                    tarea: 'Aprender c++',
+                    completado: true
+                },
+
             ];
         }
 
         this.cargarTareas();
         this.agregarEventListeners();
     }
-    agregarEventListeners(){
-        document.getElementById('recordatorio').addEventListener('keypress',(evento)=>{
-            if(evento.keyCode == 13){
+    agregarEventListeners() {
+        document.getElementById('recordatorio').addEventListener('keypress', (evento) => {
+            if (evento.keyCode == 13) {
                 this.agregarTarea(evento.target.value);
                 evento.target.value = '';
             }
         })
     }
 
+
+    cargarTareas() {
+
+    }
+
+    agregarTarea() {
+
+    }
+
+    agregarTareaClick() {
+
+    }
+
+
 }
 
 let listaTareas;
 
-window.addEventListener('load',() => {
+window.addEventListener('load', () => {
     listaTareas = new ListaTareas();
 
 })
