f65a3cf46e240a0a51a0766d6a515fb103200fa7
KristiaKnt <cristian.rojascapera5@gmail.com>
2023-08-22 23:30:42 -0500

    video numero 15 y 16

--- js/script.js
+++ js/script.js
@@ -2,6 +2,35 @@ class ListaTareas{
     constructor(){
         this.tareas=JSON.parse(localStorage.getItem('tareas'));
 
-        
+        if(!this.tareas){
+            this.tareas=[
+                {tarea: 'Aprender JavaScript',
+                completado: false},
+                {tarea: 'Aprender python',
+                completado: false},
+                {tarea: 'Aprender c++',
+                completado: true},
+                
+            ];
+        }
+
+        this.cargarTareas();
+        this.agregarEventListeners();
     }
+    agregarEventListeners(){
+        document.getElementById('recordatorio').addEventListener('keypress',(evento)=>{
+            if(evento.keyCode == 13){
+                this.agregarTarea(evento.target.value);
+                evento.target.value = '';
+            }
+        })
+    }
+
 }
+
+let listaTareas;
+
+window.addEventListener('load',() => {
+    listaTareas = new ListaTareas();
+
+})
