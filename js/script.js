39271bfa34c54b654befc8b5d667e0be2c2a3df0
KristiaKnt <cristian.rojascapera5@gmail.com>
2023-08-28 23:24:13 -0500

    videos del 20 al 25 no me funciono nada asi que a volver a verlos y enteder bien el DOM

--- js/script.js
+++ js/script.js
@@ -37,8 +37,44 @@ class ListaTareas {
         localStorage.setItem('tareas',JSON.stringify(this.tareas));
         let htmlTareas = this.tareas.reduce((html,tarea,indice)=> html += this.generarHtmlTarea(tarea,
             indice),'');
-        decodeURIComponent.getElementById('listaTareas').innerHTML =htmlTareas; 
+        decodeURIComponent.getElementById('listaTareas').innerHTML =    htmlTareas; 
     }
+    cambiarEstadoTarea(indice){
+        this.tareas[indice].completado = !this.tareas[indice].completado;
+        this.cargarTareas();
+    }
+
+    eliminarTarea(evento,indice){
+        evento.preventDefault();
+        this.tareas.splice(indice,1);
+        this.cargarTareas();
+    }
+
+    generarHtmlTarea(tarea,indice){
+        return `
+        <li class ="list-group-item checkbox">
+            <div class = "row">
+                <div class = "col-md-1 col-xs-1 col-lg-1  col-sm-1 checkbox">
+                    <label>
+                        <input id ="cambiarEstadoTarea" type="checkbox" onchange="listaTareas.cambiarEstadoTarea(${indice})"
+                        value="" class="" ${tarea.completado ? 'checked':''}>
+                    </label>
+                </div>
+                <div class = "col-md-10 col-xs-10 col-lg-10  col-sm-10 texto-tarea ${tarea.completado ? 'tareacompletada ': ''}">
+                    ${tarea.tarea}
+                </div>
+                <div class = "col-md-1 col-xs-1 col-lg-1  col-sm-1 area-icono-eliminacion">
+                    <a class="" href="/" onclick="listaTareas.eliminarTarea(event, ${indice})">
+                        <i id="eliminarTarea" data-id=${indice} class="icono-eliminacion glyphicon glyphicon-trash"></i>
+
+                    </a>
+                </div>
+            </div>
+
+        </li>
+        `;        
+    }
+
 
     agregarTarea(tarea) {
         let padre = document.getElementById('recordatorio').parentElement;
